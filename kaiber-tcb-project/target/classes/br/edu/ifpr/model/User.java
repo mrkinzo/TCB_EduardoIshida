a2a26abf89b318b672c9b21adc6a40763f22e4d7
mrKinzo <eduardokinzo@gmail.com>
2025-11-10 09:35:53 -0300

    hu

---
+++ kaiber-tcb-project/target/classes/br/edu/ifpr/model/User.java
@@ -0,0 +1,31 @@
+
+public class User {
+    public String name;
+    public String institution;
+    public String role;
+    public String password;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInstitution() {
+        return institution;
+    }
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
