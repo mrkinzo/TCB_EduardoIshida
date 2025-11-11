38aa028eef109f3492e82dc21a623eceb13d4f5d
mrKinzo <eduardokinzo@gmail.com>
2025-11-11 09:04:22 -0300

    asdfghjk

--- kaiber-tcb-project/target/classes/br/edu/ifpr/model/dao/UserDAO.java
+++ kaiber-tcb-project/target/classes/br/edu/ifpr/model/dao/UserDAO.java
@@ -6,17 +6,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class UserDAO {
-public UserDAO(){
+  public UserDAO() {
 
-}
+  }
 
- public static void cadastrarUser(User user){
- String sqlUser = "INSERT INTO user(nome, insituicao,cargo) VALUES(?,?,?)";
-      Connection con = ConnectionFactory.getConnection();
+  public static void cadastrarUser(User user) {
+    String sqlUser = "INSERT INTO user(nome, insituicao,cargo) VALUES(?,?,?)";
+    Connection con = ConnectionFactory.getConnection();
 
-       PreparedStatement psUser = con.prepareStatement(sqlUser);
-     psUser.setString(1,user.getName());
-    psUser.setString(2,user.getInstitution());
-    psUser.setString(3,user.getRole());
+    try{ PreparedStatement psUser = con.prepareStatement(sqlUser);
+    psUser.setString(1, user.getName());
+    psUser.setString(2, user.getInstitution());
+    psUser.setString(3, user.getRole());
+    psUser.executeUpdate();
+    System.out.println("----BEM VINDO----");
+ }
  }
 }
