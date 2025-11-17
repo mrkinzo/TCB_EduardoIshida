11f332e4f58b9af05a08055c85c1ea1baf1eec94
mrKinzo <eduardokinzo@gmail.com>
2025-11-17 09:39:58 -0300

    graças à Deus

--- TCB_EduardoIshida-main/kaiber-tcb-project/src/main/java/br/edu/ifpr/model/dao/MineralDAO.java
+++ TCB_EduardoIshida-main/kaiber-tcb-project/src/main/java/br/edu/ifpr/model/dao/MineralDAO.java
@@ -1,5 +1,92 @@
 package br.edu.ifpr.model.dao;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MineralDAO {
 
+    private Connection conn;
+
+    public MineralDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void inserir(Mineral m) throws SQLException {
+        String sql = "INSERT INTO minerais (tipo, dureza, cor, brilho, toxicidade, site_idsite) "
+                   + "VALUES (?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, m.getTipo());
+        stmt.setFloat(2, m.getDureza());
+        stmt.setString(3, m.getCor());
+        stmt.setString(4, m.getBrilho());
+        stmt.setString(5, m.getToxicidade());
+        stmt.setInt(6, m.getSiteIdSite());
+        stmt.executeUpdate();
+    }
+
+    public Mineral buscarPorId(int id) throws SQLException {
+        String sql = "SELECT * FROM minerais WHERE idminerais = ?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            Mineral m = new Mineral(
+                rs.getInt("idminerais"),
+                rs.getString("tipo"),
+                rs.getFloat("dureza"),
+                rs.getString("cor"),
+                rs.getString("brilho"),
+                rs.getString("toxicidade"),
+                rs.getInt("site_idsite")
+            );
+            return m;
+        }
+        return null;
+    }
+
+    public List<Mineral> listarTodos() throws SQLException {
+        String sql = "SELECT * FROM minerais";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Mineral> lista = new ArrayList<>();
+
+        while (rs.next()) {
+            lista.add(new Mineral(
+                rs.getInt("idminerais"),
+                rs.getString("tipo"),
+                rs.getFloat("dureza"),
+                rs.getString("cor"),
+                rs.getString("brilho"),
+                rs.getString("toxicidade"),
+                rs.getInt("site_idsite")
+            ));
+        }
+        return lista;
+    }
+
+    public void atualizar(Mineral m) throws SQLException {
+        String sql = "UPDATE minerais SET tipo=?, dureza=?, cor=?, brilho=?, toxicidade=?, site_idsite=? "
+                   + "WHERE idminerais=?";
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, m.getTipo());
+        stmt.setFloat(2, m.getDureza());
+        stmt.setString(3, m.getCor());
+        stmt.setString(4, m.getBrilho());
+        stmt.setString(5, m.getToxicidade());
+        stmt.setInt(6, m.getSiteIdSite());
+        stmt.setInt(7, m.getIdMinerais());
+        stmt.executeUpdate();
+    }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM minerais WHERE idminerais=?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
+    }
 }
--- TCB_EduardoIshida-main/kaiber-tcb-project/src/main/java/br/edu/ifpr/model/dao/RockDAO.java
+++ TCB_EduardoIshida-main/kaiber-tcb-project/src/main/java/br/edu/ifpr/model/dao/RockDAO.java
@@ -1,5 +1,88 @@
 package br.edu.ifpr.model.dao;
 
-public class RockDAO {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class RochaDAO {
+
+    private Connection conn;
+
+    public RochaDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void inserir(Rocha r) throws SQLException {
+        String sql = "INSERT INTO rochas (tipo, dureza, corPrincipal, ignea, site_idsite) "
+                   + "VALUES (?, ?, ?, ?, ?)";
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, r.getTipo());
+        stmt.setString(2, r.getDureza());
+        stmt.setString(3, r.getCorPrincipal());
+        stmt.setBoolean(4, r.isÍgnea());
+        stmt.setInt(5, r.getSiteIdSite());
+        stmt.executeUpdate();
+    }
+
+    public Rocha buscarPorId(int id) throws SQLException {
+        String sql = "SELECT * FROM rochas WHERE idrochas=?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            Rocha r = new Rocha(
+                rs.getInt("idrochas"),
+                rs.getString("tipo"),
+                rs.getString("dureza"),
+                rs.getString("corPrincipal"),
+                rs.getBoolean("ignea"),
+                rs.getInt("site_idsite")
+            );
+            return r;
+        }
+        return null;
+    }
+
+    public List<Rocha> listarTodos() throws SQLException {
+        String sql = "SELECT * FROM rochas";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Rocha> lista = new ArrayList<>();
+
+        while (rs.next()) {
+            lista.add(new Rocha(
+                rs.getInt("idrochas"),
+                rs.getString("tipo"),
+                rs.getString("dureza"),
+                rs.getString("corPrincipal"),
+                rs.getBoolean("ignea"),
+                rs.getInt("site_idsite")
+            ));
+        }
+        return lista;
+    }
+
+    public void atualizar(Rocha r) throws SQLException {
+        String sql = "UPDATE rochas SET tipo=?, dureza=?, corPrincipal=?, ignea=?, site_idsite=? "
+                   + "WHERE idrochas=?";
+
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setString(1, r.getTipo());
+        stmt.setString(2, r.getDureza());
+        stmt.setString(3, r.getCorPrincipal());
+        stmt.setBoolean(4, r.isÍgnea());
+        stmt.setInt(5, r.getSiteIdSite());
+        stmt.setInt(6, r.getIdRochas());
+        stmt.executeUpdate();
+    }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM rochas WHERE idrochas=?";
+        PreparedStatement stmt = conn.prepareStatement(sql);
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
+    }
 }
